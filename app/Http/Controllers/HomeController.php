739f1a447638576566f66fd96420fa6483a88a47
AYG3 <gilbertayoku@gmail.com>
2023-11-30 22:00:02 +0100

    confirmorder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -78,7 +78,7 @@ class HomeController extends Controller
             $cart->quantity = $request->quantity;
             $cart->save();
             
-            return redirect()->back()->with('messag', 'Product Added to Cart Successfully');
+            return redirect()->back()->with('message', 'Product Added to Cart Successfully');
         }
         else
         {
--- resources/views/user/showcart.blade.php
+++ resources/views/user/showcart.blade.php
@@ -28,11 +28,11 @@ https://templatemo.com/tm-546-sixteen-clothing
     <link rel="stylesheet" href="assets/css/templatemo-sixteen.css">
     <link rel="stylesheet" href="assets/css/owl.css">
 
-  @if(session()->has('messag'))
+  @if(session()->has('message'))
 
     <div class="alert alert-success">
       <button type='button' class="close" data-dismaiss="alert">X</button>
-      {{session()->get('messag')}}
+      {{session()->get('message')}}
     </div>
 
   @endif
